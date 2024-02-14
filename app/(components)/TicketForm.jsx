def618cba1150c70f03aa564750790768f01613a
Lirido1404 <102609041+Lirido1404@users.noreply.github.com>
2024-02-14 12:10:43 +0100

    1st commit

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,108 @@
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const TicketForm = () => {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    console.log("submitted");
+  };
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
+
+  return (
+    <div className="flex justify-center">
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>Create Your Ticket</h3>
+        <label>Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+
+        <label>Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows={5}
+        />
+
+        <label htmlFor="">Catégory</label>
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Hardware Problem">Hardware Problem</option>
+          <option value="Software Problems">Software Problems</option>
+          <option value="Project">Project</option>
+        </select>
+
+        <label htmlFor="">Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label htmlFor="">1</label>
+
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label htmlFor="">2</label>
+
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label htmlFor="">3</label>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TickerCard from "./(components)/TickerCard";
+
+const page = () => {
+  return (
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TickerCard />
+        <TickerCard />
+        <TickerCard />
+      </div>
+    </div>
+  );
+};
+
+export default page;
